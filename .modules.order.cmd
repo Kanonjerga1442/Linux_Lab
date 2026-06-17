278d154f68b0bd14683f02ac0d2a4d6e039d171d
vinh <kirito2k4@gmail.com>
2026-06-17 21:52:34 +0700

    Hoan thanh do an He Dieu Hanh

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo monitor_mod.o; :; } > modules.order
--- .monitor_mod.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_monitor_mod.mod := printf '%s\n'   monitor_mod.o | awk '!x[$$0]++ { print("./"$$0) }' > monitor_mod.mod
